0c73663808b8ef77cab85cd87813829a59cf3e9b
M-BENNOUR <shinobi87@hotmail.fr>
2017-06-01 22:50:34 +0200

    Retrait des commentaires inutiles dans le main

--- src/main/java/Application/main.java
+++ src/main/java/Application/main.java
@@ -8,18 +8,8 @@ import Application.Views.view;
  */
 public class main {
     public static void main(String[] args) {
-//        BufferedReader In=null;
-//
-//        try{
-//            In = new BufferedReader(new FileReader("db/test.db"));
-//            System.out.println("test");
-//            //si le fichier existe, les instructions qui suivent seront exécutées.
-//        } catch (FileNotFoundException fnfe) {
-//            System.out.println(fnfe);
-//        }
         bdd mabdd = new bdd("test.db");
         mabdd.connect();
-//        mabdd.remplirDB();
         App fen = new App(mabdd);
         mabdd.showAllCategories();
         view vue = new view();
